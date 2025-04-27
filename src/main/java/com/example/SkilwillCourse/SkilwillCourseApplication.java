1a0d31fcff06376016eecf5eab50f6672b9e07ee
lado2008 <adamialado133@gmail.com>
2025-04-27 16:46:10 +0400

    Java Spring Web Homework

---
+++ src/main/java/com/example/SkilwillCourse/SkilwillCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.SkilwillCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilwillCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilwillCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SkilwillCourse/SkilwillCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SkilwillCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilwillCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
